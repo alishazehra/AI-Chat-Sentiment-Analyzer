2f25938f8a2cba263134f744246e957aeb1eb6fc
Syeda Alisha Zehra <161215836+alishazehra@users.noreply.github.com>
2026-03-19 10:46:00 +0500

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,30 @@
 
+
+from dotenv import load_dotenv
+from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI,  set_tracing_disabled
+import os
+load_dotenv()
+set_tracing_disabled(True)
+provider= AsyncOpenAI(
+               api_key=os.getenv("GEMINI_API_KEY"),
+base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+model = OpenAIChatCompletionsModel(
+                 model="gemini-2.0-flash-exp",
+                 openai_client=provider,
+)
+
+Agent1 = Agent(
+
+                name="Assistant",
+               instructions="you are helpful assistant that solves basic math problems",
+              model=model
+)
+
+response= Runner.run_sync(
+
+                   starting_agent=Agent1,
+                   input="What is 5 +10"
+)
+
+print(response.final_output)
